404acb5a547f5d7caaf01d4ae50f840b97247244
jlingford <james.lingford@gmail.com>
2026-04-19 00:03:00 +1000

    update bash wrapper

--- run_clinker.sh
+++ run_clinker.sh
@@ -6,31 +6,10 @@
 # set input names
 # WARN: make sure there's no trailing "/"
 
-# INPUTDIR="./input/nife_select1/group3s/overlapping_group3"
-# OUTNAME="overlapping_group3"
-#
-# # set output names and dirs
-# name=${INPUTDIR##*/}
-# outname="./output/${OUTNAME}/${name}"
-# trimdir="./output/${OUTNAME}/trimmed_gbks"
-# mkdir -p "./output/${OUTNAME}"
-# mkdir -p "${trimdir}"
-#
-# # run clinker with trimming
-# clinker \
-#     "$INPUTDIR" \
-#     -p "${outname}.html" \
-#     -s "${outname}.json" \
-#     -mo "${outname}.csv" \
-#     -o "${outname}_ali.tsv" \
-#     -dl "," \
-#     -t "$trimdir" \
-#     -f
-
-#####
-
-INPUTDIR="./output/overlapping_group3/trimmed_gbks"
-OUTNAME="overlapping_group3_trim"
+# INPUTDIR="./input/nife_select1/group4s/overlapping_group4"
+# OUTNAME="overlapping_group4_test3"
+INPUTDIR="./input/nife_select1/group3s/overlapping_group3"
+OUTNAME="overlapping_group3_redo"
 
 # set output names and dirs
 name=${INPUTDIR##*/}
@@ -39,12 +18,29 @@ trimdir="./output/${OUTNAME}/trimmed_gbks"
 mkdir -p "./output/${OUTNAME}"
 mkdir -p "${trimdir}"
 
-# run clinker with no trimming
+# run clinker with trimming
 clinker \
     "$INPUTDIR" \
     -p "${outname}.html" \
     -s "${outname}.json" \
     -mo "${outname}.csv" \
     -o "${outname}_ali.tsv" \
+    -dl "," \
+    -t "$trimdir" \
+    -f
+
+#########################################
+# running on trimmed gbk files now...
+#########################################
+
+INPUTDIR="${trimdir}"
+
+# run clinker with no trimming
+clinker \
+    "$INPUTDIR" \
+    -p "${outname}.trimmed.html" \
+    -s "${outname}.trimmed.json" \
+    -mo "${outname}.trimmed.csv" \
+    -o "${outname}_ali.trimmed.tsv" \
     -dl "," \
     -f
