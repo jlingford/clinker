6c8a69e73b09065911ef414c36ef97cb2ffb7564
jlingford <james.lingford@gmail.com>
2026-04-27 17:08:16 +1000

    update run_clinker.sh

--- run_clinker.sh
+++ run_clinker.sh
@@ -6,31 +6,31 @@
 # set input names
 # WARN: make sure there's no trailing "/"
 
-# INPUTDIR="./input/nife_select1/group3s/overlapping_group3"
-# OUTNAME="overlapping_group3"
-#
-# # set output names and dirs
-# name=${INPUTDIR##*/}
-# outname="./output/${OUTNAME}/${name}"
-# trimdir="./output/${OUTNAME}/trimmed_gbks"
-# mkdir -p "./output/${OUTNAME}"
-# mkdir -p "${trimdir}"
-#
-# # run clinker with trimming
-# clinker \
-#     "$INPUTDIR" \
-#     -p "${outname}.html" \
-#     -s "${outname}.json" \
-#     -mo "${outname}.csv" \
-#     -o "${outname}_ali.tsv" \
-#     -dl "," \
-#     -t "$trimdir" \
-#     -f
+INPUTDIR="./input/group2a_sliced_genbanks"
+OUTNAME="group2a_sliced_round1"
+
+# set output names and dirs
+name=${INPUTDIR##*/}
+outname="./output/${OUTNAME}/${name}"
+trimdir="./output/${OUTNAME}/trimmed_gbks"
+mkdir -p "./output/${OUTNAME}"
+mkdir -p "${trimdir}"
+
+# run clinker with trimming
+clinker \
+    "$INPUTDIR" \
+    -p "${outname}.html" \
+    -s "${outname}.json" \
+    -mo "${outname}.csv" \
+    -o "${outname}_ali.tsv" \
+    -dl "," \
+    -t "$trimdir" \
+    -f
 
 #####
 
-INPUTDIR="./output/overlapping_group3/trimmed_gbks"
-OUTNAME="overlapping_group3_trim"
+INPUTDIR=$OUTNAME
+OUTNAME="group2a_sliced_round2"
 
 # set output names and dirs
 name=${INPUTDIR##*/}
